29021b2b0f2bf0336a74c796ee3be944e6517f23
Allie <25503691+GalaxyAllie@users.noreply.github.com>
2021-07-20 22:04:16 +0100

    initial board folder for justin

---
+++ boards/justin_c_u_b_e/board_config.h
@@ -0,0 +1,22 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "AllieCat"
+#define PRODUCT_NAME "Candy the Catacorn"
+#define VOLUME_LABEL "CANDYBOOT"
+#define INDEX_URL "https://github.com/MermaidAllie/Candy-the-Catacorn"
+#define BOARD_ID "SAMD21E18A-Candy"
+
+#define USB_VID 0x04D8
+#define USB_PID 0xEC2F
+
+#define LED_PIN PIN_PA10
+//#define LED_TX_PIN PIN_PA27
+//#define LED_RX_PIN PIN_PB03
+
+#define BOARD_RGBLED_CLOCK_PIN            PIN_PA01
+#define BOARD_RGBLED_DATA_PIN             PIN_PA00
+
+#endif
